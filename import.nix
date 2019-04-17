29b81d7762578ee9cc7cc2866d5c2ffdfe4f128f
Clayton Voges <vogesclayton@gmail.com>
2019-04-17 08:41:32 -0500

    Adding import of home-manager to Personal

--- import.nix
+++ import.nix
@@ -3,14 +3,20 @@
 {
   imports = [
     ./docs.nix
+    ./environment.nix
     ./fonts.nix
     ./hardware.nix
+    ./home.nix
+    ./i18n.nix
     ./nix.nix
     ./programs.nix
     ./security.nix
+    ./services.nix
     ./sound.nix
     ./systemd.nix
     ./time.nix
+    ./users.nix
     ./xdg.nix
+    (import (builtins.fetchTarball https://github.com/rycee/home-manager/archive/release-18.09.tar.gz) {}).nixos
   ];
 }
