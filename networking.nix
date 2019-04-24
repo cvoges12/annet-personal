898cf492291b6e9fd2102dec6ead556befeb6498
Clayton Voges <38054771+cvoges12@users.noreply.github.com>
2019-04-24 09:12:39 -0500

    Create networking.nix

---
+++ networking.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+
+{
+  networking = {
+    firewall = {
+      allowPing = true;
+      allowedTCPPorts = [ 22 53 80 443 ];
+      allowedUDPPorts = [ 53 67 68 80 123 433 ];
+      # shorten to allowed number of mosh connections
+      # uncomment when setting up mosh
+      #allowedUDPPortRanges = [
+      #  { from = 60000;
+      #    to = 61000; }
+      #];
+      checkReversePath = true;
+      logRefusedConnections = true;
+      logRefusedPackets = true;
+      logRefusedUnicastsOnly = true;
+      logReversePathDrops = true;
+      pingLimit = "--limit 1/minute --limit-burst 5";
+    };
+    nameservers = [ "1.1.1.1" ];
+    usePredictableInterfaceNames = true;
+  };
+}
