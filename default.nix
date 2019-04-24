248727c6c1136b0a51a236453d5aaef384a1ec7d
cvoges12 <vogesclayton@gmail.com>
2019-04-24 10:05:47 -0500

    third try at importing

--- default.nix
+++ default.nix
@@ -1,23 +1,25 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
-  imports = [
-    ./docs.nix
-    ./environment.nix
-    ./fonts.nix
-    ./hardware.nix
-    ./home.nix
-    ./i18n.nix
-    ./networking.nix
-    ./nix.nix
-    ./programs.nix
-    ./security.nix
-    ./services.nix
-    ./sound.nix
-    ./systemd.nix
-    ./time.nix
-    ./users.nix
-    ./xdg.nix
-    (import (builtins.fetchTarball https://github.com/rycee/home-manager/archive/release-18.09.tar.gz) {}).nixos
-  ];
+  config = {
+    imports = [
+      ./docs.nix
+      ./environment.nix
+      ./fonts.nix
+      ./hardware.nix
+      ./home.nix
+      ./i18n.nix
+      ./networking.nix
+      ./nix.nix
+      ./programs.nix
+      ./security.nix
+      ./services.nix
+      ./sound.nix
+      ./systemd.nix
+      ./time.nix
+      ./users.nix
+      ./xdg.nix
+      (import (builtins.fetchTarball https://github.com/rycee/home-manager/archive/release-18.09.tar.gz) {}).nixos
+    ];
+  };
 }
