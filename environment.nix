2bdd899072611f4fcc88522095c8d20014fb9109
Clayton Voges <38054771+cvoges12@users.noreply.github.com>
2019-04-25 11:35:33 -0500

    Update environment.nix

--- environment.nix
+++ environment.nix
@@ -5,6 +5,11 @@
     sessionVariables = {
       EDITOR = "nvim";
     };
+    shellInit = ''
+      export GPG_TTY="$(tty)"
+      gpg-connect-agent /bye
+      export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+    '';
     systemPackages = with pkgs; [
 
       # Applications
